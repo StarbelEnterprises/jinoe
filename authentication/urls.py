2c28e7940cb7a694298a17f332a9bc7ad6fc74e8
isayaeli <isayaelib@gmail.com>
2022-07-12 07:49:03 +0300

    added course selection skeleton

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.landing, name='landing'),
     path('login/',  views.login , name='login'),
+    path('home/',  views.home , name='home'),
     path('register/',  views.register , name='register'),
     path('logout/',  views.login , name='logout'),
     
