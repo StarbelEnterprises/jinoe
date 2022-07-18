304e2bcbed4e578475307b74540e4fd9778e2940
Nduke <jnduke@datavision.co.tz>
2022-07-18 16:38:18 +0300

    -[core fix] - Add field create_by to enrollment     - Alter field create_at on enrollment     - Alter field updated_at on enrollment     - Alter field create_at on levels     - Alter field updated_at on levels     - Alter field create_at on modules     - Alter field updated_at on modules     - Alter field create_at on subjects     - Alter field progress_status on subjects     - Alter field updated_at on subjects     - Alter field create_at on subtopics     - Alter field sub_topic_status on subtopics     - Alter field updated_at on subtopics     - Alter field create_at on topics     - Alter field topic_status on topics     - Alter field updated_at on topics .[interactive fix] - Alter field create_at on answerlogs - Alter field updated_at on answerlogs - Alter field create_at on answeroptions - Alter field updated_at on answeroptions - Alter field create_at on questions - Alter field updated_at on questions - Alter field create_at on topiclikes - Alter field updated_at on topiclikes - Alter field create_at on topicrates - Alter field updated_at on topicrates - [weninar fix] Add field pioneer_category to webinerpioneers

--- core/models.py
+++ core/models.py
@@ -5,13 +5,14 @@ from django.contrib.auth.models import User
 from datetime import datetime, timezone
 from authentication.models import UserProfile
 from enum import Enum
+from django.utils.translation import gettext_lazy as _
 
 class Levels(models.Model):
     order_by = models.IntegerField(null=True,)
     userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE,)
     name = models.CharField(max_length=60, null= True , blank=True)
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)
     
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lv_created_by', null=True)
 
@@ -26,8 +27,9 @@ class Levels(models.Model):
 class Enrollment(models.Model):
     level = models.ForeignKey(Levels, on_delete=models.CASCADE, related_name='enrolled_to_set')
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrolled_student_set')
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)
+    create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrol_created_by', null=True)
     class Meta:
         verbose_name_plural = 'Enrolled Education level'
         db_table = 'jinoe_enrolled_eduction_level'
@@ -38,8 +40,8 @@ class Enrollment(models.Model):
 class Modules(models.Model):
     level = models.ForeignKey(Levels, on_delete=models.CASCADE)
     name = models.CharField(max_length= 60, null= True, blank=True)
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)
     
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='md_created_by', null=True)
 
@@ -54,16 +56,16 @@ class Subjects(models.Model):
     module = models.ForeignKey(Modules, on_delete=models.CASCADE)
     name = models.CharField(max_length=60,null=True,blank=True)
     subject_type = models.CharField(max_length=60, null=True,blank=True)
-    STATUS_CHOICES = (
-    ('ST', 'STARTED'),
-    ('NST', 'NOT_STARTED'),
-    ('DL', 'DELAYED'),
-    ('CMP', 'COMPLETED'),
-    ('DISC', 'DISQUALIFIED'),
-    )
-    progress_status = models.CharField(max_length=30,choices=STATUS_CHOICES)
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)    
+
+    class SubjectsProgressStatuses(models.TextChoices):
+         NOT_STARTED = 'NST',_('Not_Started')
+         STARTED = 'ST',_('Started')
+         DELAYED  = 'DL',_('Delayed')
+         COMPLETED = 'CMP',_('Completed')
+
+    progress_status = models.CharField(max_length=30,choices=SubjectsProgressStatuses.choices,default=SubjectsProgressStatuses.NOT_STARTED,)
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)   
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sbj_created_by', null=True)
 
 
@@ -77,17 +79,17 @@ class Subjects(models.Model):
 class Topics(models.Model):
     subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
     name = models.CharField(max_length=60,null=True,blank=True)
-    STATUS_CHOICES = (
-    ('ST', 'STARTED'),
-    ('NST', 'NOT_STARTED'),
-    ('DL', 'DELAYED'),
-    ('CMP', 'COMPLETED'),
-    ('DISC', 'DISQUALIFIED'),
-    )
-    topic_status = models.CharField(max_length=30,choices=STATUS_CHOICES,default='NOT_STARTED')
+
+    class TopicsProgressStatuses(models.TextChoices):
+         NOT_STARTED = 'NST',_('Not_Started')
+         STARTED = 'ST',_('Started')
+         DELAYED  = 'DL',_('Delayed')
+         COMPLETED = 'CMP',_('Completed')
+
+    topic_status = models.CharField(max_length=30,choices=TopicsProgressStatuses.choices,default=TopicsProgressStatuses.NOT_STARTED,)
     media = models.CharField(max_length=200,null=True,blank=True)
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)    
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)   
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topic_created_by', null=True)
     
     class Meta:
@@ -100,17 +102,17 @@ class Topics(models.Model):
 class SubTopics(models.Model):
     topic = models.ForeignKey(Topics, on_delete=models.CASCADE)
     name = models.CharField(max_length=60,null=True,blank=True)
-    STATUS_CHOICES = (
-    ('ST', 'STARTED'),
-    ('NST', 'NOT_STARTED'),
-    ('DL', 'DELAYED'),
-    ('CMP', 'COMPLETED'),
-    ('DISC', 'DISQUALIFIED'),
-     )
-    sub_topic_status = models.CharField(max_length=30,choices=STATUS_CHOICES,default='NOT_STARTED')
+
+    class SubTopicsProgressStatuses(models.TextChoices):
+         NOT_STARTED = 'NST',_('Not_Started')
+         STARTED = 'ST',_('Started')
+         DELAYED  = 'DL',_('Delayed')
+         COMPLETED = 'CMP',_('Completed')
+
+    sub_topic_status = models.CharField(max_length=30,choices=SubTopicsProgressStatuses.choices,default=SubTopicsProgressStatuses.NOT_STARTED,)
     media = models.CharField(max_length=200,null=True,blank=True)
-    create_at = models.DateTimeField(auto_created=True, null = True)
-    updated_at = models.DateField(default=datetime.now)    
+    create_at = models.DateTimeField(auto_created=True, null = True,editable=False)
+    updated_at = models.DateField(auto_now=True)   
     create_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sub_topic_created_by', null=True)
 
     class Meta:
@@ -120,7 +122,6 @@ class SubTopics(models.Model):
     def __str__(self):
         return str(self.name)
 
-# class Subjects(models.model):
     
 
 
