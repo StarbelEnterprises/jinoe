d398ede5490606b46821ead084973a94929ab300
jenadiusnicholaus <ucode888@gmail.com>
2022-07-28 18:26:39 +0300

    [feat], update module deatails

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('welcome/',  views.welcome , name='welcome'),
-    path('single-module/',  views.single_module , name='single_module'),
+    path('single-module/<int:pk>',  views.ModuleDetails.as_view() , name='single_module'),
 ]
