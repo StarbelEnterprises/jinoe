87ce284c6c2d0d808bae337c24c623e4b595a284
jenadiusnicholaus <ucode888@gmail.com>
2022-07-31 16:25:13 +0300

    [feat] add big video player

---
+++ core/static/dashboard/assets/js/jinoe_custom_video_player.js
@@ -0,0 +1,38 @@
+let videoElem = document.getElementById("intro-video");
+let playButton = document.getElementById("intro-video-play-btn");
+
+playButton.addEventListener('click',  handlePlaybtn, false)
+// playVideo();
+// handlePlaybtn()
+
+if (videoElem.paused) {
+  playVideo();
+  playButton.style.display="block"
+} else {
+  // videoElem.pause();
+  playButton.style.display="none"
+}
+
+
+async function playVideo() {
+    try {
+      playButton.style.display="none"
+      await videoElem.play();
+      conosole.log(' playing')
+    
+    } catch(err) {
+      console.log('some error happening')
+      playButton.classList.remove("playing");
+    }
+  }
+  
+function handlePlaybtn(){
+    if (videoElem.paused) {
+      playVideo();
+      playButton.style.display="none"
+    } else {
+      videoElem.pause();
+      playButton.style.display="none"
+    }
+  }
+
