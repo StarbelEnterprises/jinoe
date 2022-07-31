1984894c1d0a2f55d94f6e2615df79ce3b6ec334
jenadiusnicholaus <ucode888@gmail.com>
2022-07-31 23:32:40 +0300

    update module details

--- core/views.py
+++ core/views.py
@@ -38,20 +38,18 @@ def welcome(request):
 class ModuleDetails(View):
     def get(self, request, pk, *args, **argus):
         module =  Modules.objects.get(pk=pk)
-       
+        enrolled_level = Enrollment.objects.filter(student=request.user).first()
+        enrolled_modules = enrolled_level.level.get_modules() 
         context = {
-        'module': module
+        'module': module,
+        'enrolled_module':  enrolled_modules
         }
-
         return render(request, template_name='dashboard/module_single.html',context=context)
 
     def post(self, request, pk, *args, **argus):
         print( request.POST.get('id'))
         subtopic = get_object_or_404(SubTopics, id=request.POST.get('id'))
         subsubtopic = serializers.serialize("json", SubSubTopic.objects.filter(sub_sub_topic_id=subtopic))
-    
-        
-    
         response = {
             'success': 'true',
             'data':subsubtopic,
