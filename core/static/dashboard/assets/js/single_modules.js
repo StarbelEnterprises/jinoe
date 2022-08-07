02f94240c7caf446666e6b0134852f8d38ed387e
isayaeli <isayaelib@gmail.com>
2022-08-07 10:02:21 +0300

    added single curriculum page

--- core/static/dashboard/assets/js/single_modules.js
+++ core/static/dashboard/assets/js/single_modules.js
@@ -1,12 +1,12 @@
 
 
-//  const allsidebarItems = document.getElementsByClassName("sidebar-link")
-//  for(let i = 0; i < allsidebarItems.length; i++ ){
-//     allsidebarItems[i].addEventListener('click', function(){
-//         console.log(allsidebarItems[i])
-//         let active = document.getElementsByClassName('current');
-//         console.log(active)
-//             active[0].className = active[0].className.replace('current', "")
-//             this.className += " current";
-//     });
-// }
+ const allsidebarItems = document.getElementsByClassName("sidebar-link")
+ for(let i = 0; i < allsidebarItems.length; i++ ){
+    allsidebarItems[i].addEventListener('click', function(){
+        console.log(allsidebarItems[i])
+        let active = document.getElementsByClassName('current');
+        console.log(active)
+            active[0].className = active[0].className.replace('current', "")
+            this.className += " current";
+    });
+}
--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('welcome/',  views.welcome , name='welcome'),
     path('single-module/<int:pk>',  views.ModuleDetails.as_view() , name='single_module'),
- 
+    path('single-curriculum/<int:pk>',  views.CurriculumDetails.as_view() , name='single_curriculum'),
 ]
