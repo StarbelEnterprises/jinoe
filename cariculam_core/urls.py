8dc3c97cf2ac144fc0759638373027f66b489d19
jenadiusnicholaus <ucode888@gmail.com>
2022-08-07 18:49:10 +0300

    [feat], adding  curriculum logic

--- cariculam_core/urls.py
+++ cariculam_core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+       path('single-curriculum/<int:pk>',  views.CurriculumDetails.as_view() , name='single_curriculum'),
 
 ]
